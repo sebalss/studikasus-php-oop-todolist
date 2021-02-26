77ffee5d090b4025cc00a80af415bad39c2202d8
Mochammad Iqbal Iskandar <74407184+sebalss@users.noreply.github.com>
2021-02-26 15:26:47 +0700

    Delete TestViewShowTodoList.php

--- Test/TestViewShowTodoList.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "../View/ViewShowTodoList.php";
-require_once "../BusinessLogic/AddTodoList.php";
-
-addTodoList("Eko");
-addTodoList("Khennedy");
-addTodoList("Mochammad");
-addTodoList("Ilham");
-viewShowTodoList();
